9d735d3edd753b6c47cae953012d0bef5321c316
=Jake Varley <jakevarley@gmail.com>
2017-07-25 10:04:56 -0400

    adding model download script

---
+++ trained_models/download_trained_model.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget http://shapecompletiongrasping.cs.columbia.edu/static/depth_y17_m05_d26_h14_m22_s35_bare_keras_v2.tar.gz
+tar -xvf depth_y17_m05_d26_h14_m22_s35_bare_keras_v2.tar.gz
