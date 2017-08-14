e9be716f4e55a7d21532836a84a67f9da548659b
Jake Varley <jakevarley@gmail.com>
2017-08-14 14:13:30 -0400

    Update download_trained_model.sh

--- scripts/shape_completion_server/trained_models/download_trained_model.sh
+++ scripts/shape_completion_server/trained_models/download_trained_model.sh
@@ -2,3 +2,4 @@
 
 wget http://shapecompletiongrasping.cs.columbia.edu/static/depth_y17_m05_d26_h14_m22_s35_bare_keras_v2.tar.gz
 tar -xvf depth_y17_m05_d26_h14_m22_s35_bare_keras_v2.tar.gz
+touch depth_y17_m05_d26_h14_m22_s35_bare_keras_v2/__init__.py
